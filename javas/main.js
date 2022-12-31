2a5933ed400731818481b969d60468c02e4fed1c
Swarna Barua <121425868+Swarna00@users.noreply.github.com>
2022-12-31 04:14:48 -0800

    Add files via upload
    hi

---
+++ javas/main.js
@@ -0,0 +1,71 @@
+/*
+//var
+
+//var name= "Swarna Barua",
+//    roll=367874 ;
+//    name="tuhin";
+
+//let
+
+//let name="swarna";
+//     name= "Tuhin";
+//let roll=588791;
+//    roll= 45666;  
+
+//const
+
+const name="swarna barua";
+//       name="barna";
+const roll= 374547;
+//      roll=4955;       
+console.log(name , roll)    
+*/
+//operator
+
+/*
+let number1=20;
+let number2=20;
+console.log(number1+number2)  
+console.log(number1-number2)   
+console.log(number1*number2) 
+console.log(number1/number2) 
+console.log(number1%number2) 
+
+let firstName="Swarna";
+let lastName="Barua";
+document.write(firstName +" "+lastName)
+
+document.write("Name:Swarna Barua<br>");
+document.write("Roll:588791<br>");
+document.write("Department:Computer<br>");
+document.write("Group:B1<br>");
+
+*/
+
+document.getElementById("small_scren").innerHTML = "shunu";
+
+//object
+/*
+const personDetails={
+       students1 : {
+       Name:"Swarna Barua",
+       Roll:588791,
+       Department:"Computer"
+       },
+       students2 : {
+              Name:{
+                   firstName:"Swarna",
+                   lastName:"Barua",  
+              },
+              Roll:588791,
+              Department:"Computer"
+              }
+}
+let studentFullName=personDetails.students2.Name.firstName;
+let studentFullName2=personDetails.students2.Name.lastName;
+console.log(studentFullName +" "+ studentFullName2)
+//console.log(personDetails.students2.lastName)
+
+//console.warn(personDetails)
+*/
+  
